1c88a5dc9b9c70e28d2133a85b31948b7756861f
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-12-09 13:18:09 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -7,13 +7,16 @@ function App() {
 
   // Jokes state
   const [jokes, setJokes] = useState([]);
+  const [isLoading, setIsLoading] = useState()
 
 
   // Fetch Jokes
   async function fetchJokesHandler(){
+    setIsLoading(true)
     const responsed =  await fetch('https://official-joke-api.appspot.com/random_ten')
     const jokesData = await responsed.json();
     setJokes(jokesData);
+    setIsLoading(false)
   }
 
 
@@ -23,7 +26,7 @@ function App() {
       <button onClick={fetchJokesHandler}>Fetch Jokes</button>
     </section>
     <section className='jokes'>
-      <JokeList jokes={jokes}/>
+      {isLoading ? <h1>Loading...</h1> : <JokeList jokes={jokes}/>}
     </section>
     </>
   );
