1ea17b0a9492d0c422a0e07b6e89287f83fe7c27
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-12-19 15:51:01 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -8,14 +8,24 @@ function App() {
   // Jokes state
   const [jokes, setJokes] = useState([]);
   const [isLoading, setIsLoading] = useState()
-
+  const [error, setError] = useState(null);
 
   // Fetch Jokes
   async function fetchJokesHandler(){
     setIsLoading(true)
-    const responsed =  await fetch('https://official-joke-api.appspot.com/random_ten')
-    const jokesData = await responsed.json();
-    setJokes(jokesData);
+    setError(null);
+    // Catch error
+    try {
+      const responsed =  await fetch('https://official-joke-api.appspot.com/random_te')
+      // If responsed data is false throw error
+      if(!responsed.ok){
+        throw new Error('Something went wrong');
+      }
+      const jokesData = await responsed.json();
+      setJokes(jokesData);
+    } catch (e) {
+      setError(e.message)
+    }
     setIsLoading(false)
   }
 
@@ -26,7 +36,8 @@ function App() {
       <button onClick={fetchJokesHandler}>Fetch Jokes</button>
     </section>
     <section className='jokes'>
-      {isLoading ? <h1>Loading...</h1> : <JokeList jokes={jokes}/>}
+      {isLoading && jokes.length === 0 ? <h1>Loading...</h1> : <JokeList jokes={jokes}/>}
+      {!isLoading && error && <p>{error}</p>}
     </section>
     </>
   );
